b5617bf9b4b7d43b342ebe5537791adb0dc89eed
Rishit007 <74972122+Rishit007@users.noreply.github.com>
2021-01-08 15:33:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg, ground;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,500);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600,500,1200,20);
+ 
+    box1 = new Box(900,100,70,70);
+    box2 = new Box(900,100,70,70);
+    box3 = new Box(900,100,70,70);
+    box4 = new Box(900,100,70,70);
+    box5 = new Box(900,100,70,70);
+    box6 = new Box(900,100,70,70);
+    
+    box7 = new Box(800,100,70,70);
+    box8 = new Box(800,100,70,70);
+    box9 = new Box(800,100,70,70);
+    box10= new Box(800,100,70,70);
+    box11= new Box(800,100,70,70);
+    box12= new Box(800,100,70,70);
+
+    ball = new Ball(500,200,60,60);
+
+    rope = new Rope(ball.body,{x:500, y:50});
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    ground.display();
+    
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+
+    ball.display();
+ 
+    rope.display();    
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(ball.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    rope.fly();
+    gameState = "launched";
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=0600 && hour<=1900){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
